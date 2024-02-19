d17d535c488a7249ff24f938d387083f9f3be51a
Freejinn <141774931+Freejinn@users.noreply.github.com>
2024-02-18 18:39:24 -0700

    carousel and programs visual appeal

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class CarouselComponent {
 
+  // <div id="carousel" class="carousel slide" data-ride="carousel">
+  //   <div class="carousel-inner">
+  //     <div class="carousel-item">
+  //       <img src="/assets/images/armbar.jpg" class="d-block w-100" alt="...">
+  //     </div>
+  //     <div class="carousel-item active">
+  //       <img src="/assets/images/kidClass2.JPG" class="d-block w-100" alt="...">
+  //     </div>
+  //     <div class="carousel-item">
+  //       <img src="/assets/images/knife.jpg" class="d-block w-100" alt="/assets/images/armbar.jpg">
+  //     </div>
+  //     <div class="carousel-item">
+  //       <img src="/assets/images/lesson1.jpg" class="d-block w-100" alt="...">
+  //     </div>
+  //   </div>
+  //   <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+  //     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  //     <span class="visually-hidden">Previous</span>
+  //   </button>
+  //   <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+  //     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  //     <span class="visually-hidden">Next</span>
+  //   </button>
+  // </div>
+
 }
